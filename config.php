176effc49da06509a07b841660ebc8cd19d42dac
fsiggor <fsiggor@gmail.com>
2026-02-12 21:24:53 -0300

    feat(ui): implement SCSS loading logic and RN flag color palette
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- config.php
+++ config.php
@@ -10,3 +10,4 @@ $THEME->rendererfactory = 'theme_overridden_renderer_factory';
 $THEME->requiredblocks = '';
 $THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
 $THEME->prescsscallback = 'theme_demolaypotiguar_get_pre_scss';
+$THEME->extrascsscallback = 'theme_demolaypotiguar_get_extra_scss';
--- lib.php
+++ lib.php
@@ -2,5 +2,18 @@
 defined('MOODLE_INTERNAL') || die();
 function theme_demolaypotiguar_get_pre_scss($theme) {
     global $CFG;
-    return file_get_contents($CFG->dirroot . '/theme/demolaypotiguar/scss/pre.scss');
+    $content = '';
+    if (file_exists($CFG->dirroot . '/theme/demolaypotiguar/scss/pre.scss')) {
+        $content = file_get_contents($CFG->dirroot . '/theme/demolaypotiguar/scss/pre.scss');
+    }
+    return $content;
+}
+
+function theme_demolaypotiguar_get_extra_scss($theme) {
+    global $CFG;
+    $content = '';
+    if (file_exists($CFG->dirroot . '/theme/demolaypotiguar/scss/post.scss')) {
+        $content = file_get_contents($CFG->dirroot . '/theme/demolaypotiguar/scss/post.scss');
+    }
+    return $content;
 }
